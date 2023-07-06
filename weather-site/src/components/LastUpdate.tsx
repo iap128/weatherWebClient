aaf66a011ff445a234ad8267b2b8c6107dc0966b
iap128 <103337777+iap128@users.noreply.github.com>
2023-07-06 13:39:03 -0700

    Add files via upload

---
+++ weather-site/src/components/LastUpdate.tsx
@@ -0,0 +1,46 @@
+import { ReloadOutlined } from "@ant-design/icons";
+import { Button, Typography } from "antd";
+import { useEffect, useState } from "react";
+import { fetchCardData } from "../constants/CardProvider";
+
+const LastUpdate = () => {
+    const [lastUpdate, setLastUpdate] = useState('2');
+
+    useEffect(() => {
+        const getData = async () => {
+            //all entries contain the time added, so we can choose any
+            //I'm just picking temperature and reading from the date field
+            const data = await fetchCardData('temperature');
+            setLastUpdate(data[0]?.date);
+        }
+
+        getData();
+    }, []);
+
+    let agoString = '';
+    let diff = 0;
+
+    if (lastUpdate) {
+        const now = new Date();
+        const then = new Date(lastUpdate);
+        diff = Math.round((now.getTime() - then.getTime()) / 60000);
+        
+        if (diff < 60) {
+            agoString = ' seconds ago';
+        }
+        else {
+            agoString = ' minutes ago';
+        }
+    }
+
+    return (
+        <div>
+            <Typography.Text>Last update: </Typography.Text>
+            <Typography.Text type="secondary">{diff}</Typography.Text>
+            <Typography.Text type="secondary">{agoString}</Typography.Text>
+            <Button icon={<ReloadOutlined />} style={{margin: '5px'}}/>
+        </div>
+    );
+};
+
+export default LastUpdate;
