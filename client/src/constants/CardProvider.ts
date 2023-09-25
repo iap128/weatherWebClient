e663cdc7cfbdaa6f82f6729b96a193326d282354
iap128 <103337777+iap128@users.noreply.github.com>
2023-09-25 14:08:09 -0700

    Got docker builds working

--- client/src/constants/CardProvider.ts
+++ client/src/constants/CardProvider.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const fetchCardData = async (dataType: string) => {
   try {
-    const res = await axios.get(`http://localhost:3306/${dataType}`);
+    const res = await axios.get(`http://localhost:4002/${dataType}`);
     return res.data;
   } catch (err) {
     console.log(err);
